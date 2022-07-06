c423e47fb6d4ce4cdc88c7ef6622093b2cb63346
Minh Chau <ducmk254@gmail.com>
2022-07-06 22:01:27 +0700

    defaultlayout base css

--- src/components/Layouts/DefaultLayout/index.jsx
+++ src/components/Layouts/DefaultLayout/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import classNames from 'classnames/bind';
 import Header from '~/components/Layouts/components/Header'
 import Sidebar from '~/components/Layouts/components/Sidebar'
 
+import styles from "./DefaultLayout.module.scss";
+
+const cx = classNames.bind(styles);
 
 export default function DefaultLayout({ children }) {
     return (
-        <>
+        <div className={cx('container')}>
             <Header />
-            <div className='container'>
+            <div className={cx('wrapper')}>
                 <Sidebar />
-                <div className="content">
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/components/Layouts/components/Sidebar/index.jsx
+++ src/components/Layouts/components/Sidebar/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import classNames from 'classnames/bind'
+
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function Sidebar() {
     return (
-        <div>Sidebar</div>
+        <div className={cx('sidebar')}>
+            Sidebar
+        </div>
     )
 }
